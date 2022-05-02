597903d26cadba72a15b12a34bbee532989147bc
sol namgung <nandaras0103@gmail.com>
2022-05-02 23:08:25 +0900

    added constructor JFrame

--- src/com/javaWithGUI/GamePanel.java
+++ src/com/javaWithGUI/GamePanel.java
@@ -1,4 +1,16 @@
 package com.javaWithGUI;
 
-public class GamePanel {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel() {
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
--- src/com/javaWithGUI/SnakeGame.java
+++ src/com/javaWithGUI/SnakeGame.java
@@ -1,4 +1,8 @@
 package com.javaWithGUI;
 
 public class SnakeGame {
+
+    public static void main(String[] args) {
+        new GameFrame();
+    }
 }
